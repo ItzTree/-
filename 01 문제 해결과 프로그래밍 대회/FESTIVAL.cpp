be2a475f101ca609033a004995e8357a37fbdb81
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-09-03 12:25:28 +0900

    Update FESTIVAL.cpp

--- 01 문제 해결과 프로그래밍 대회/FESTIVAL.cpp
+++ 01 문제 해결과 프로그래밍 대회/FESTIVAL.cpp
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main()
+{
+    int c;
+
+    scanf("%d", &c);
+    while(c--) {
+        int n, l;
+        int cost[1000] = {};
+        double ans = 100;
+
+        scanf("%d %d", &n, &l);
+        for(int i=0; i<n; i++) {
+            scanf("%d", &cost[i]);
+        }
+
+        for(int i=0; i<n; i++) {
+            int sum = 0;
+            for(int j=i; j<n; j++) {
+                sum += cost[j];
+                if(j-i+1 >= l) {
+                    double res = (double)sum / (j-i+1);
+                    ans = ans > res ? res : ans;
+                }
+            }
+        }
+        printf("%.8lf\n", ans);
+    }
+}
